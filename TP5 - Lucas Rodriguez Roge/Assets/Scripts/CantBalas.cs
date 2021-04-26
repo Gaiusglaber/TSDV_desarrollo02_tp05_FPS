6eb325baa42f9397b566ed051c14eb3b4425c2d5
Gaiusglaber <lucas.rodriguez.roge@gmail.com>
2021-04-26 11:36:26 -0300

    Basic gunfire system added

---
+++ TP5 - Lucas Rodriguez Roge/Assets/Scripts/CantBalas.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CantBalas : MonoBehaviour
+{
+    public Gun gun;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<TMPro.TMP_Text>().text = "" + gun.balas;
+    }
+}
---
+++ TP5 - Lucas Rodriguez Roge/Assets/Scripts/Gun.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Gun : MonoBehaviour
+{
+    private float distance = 5.0f;
+    public int balas = 15;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (balas > 0)
+            {
+                Shoot();
+                balas--;
+            }
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, distance);
+
+    }
+}
