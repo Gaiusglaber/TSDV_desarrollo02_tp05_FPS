f31d6e183f65231d2cfabfd4b701e04ac8fd79b4
Gaiusglaber <lucas.rodriguez.roge@gmail.com>
2021-05-12 11:11:51 -0300

    Rocket fires granades

--- TP5 - Lucas Rodriguez Roge/Assets/Scripts/RocketGun.cs
+++ TP5 - Lucas Rodriguez Roge/Assets/Scripts/RocketGun.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class RocketGun : MonoBehaviour
 {
+    public GameObject ball;
+    public Transform spawnPoint;
+    public float range = 15f;
     public void Appear()
     {
         Debug.Log("Estoy mostrando la arma cohete");
@@ -22,6 +25,14 @@ public class RocketGun : MonoBehaviour
         if (Input.GetKeyUp(KeyCode.Alpha1))
         {
             gameObject.SetActive(false);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject instanciateBall= Instantiate(ball,
+                new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z),
+                Quaternion.identity);
+            instanciateBall.GetComponent<Rigidbody>().AddForce(spawnPoint.forward * range, ForceMode.Impulse);
         }
     }
 }
